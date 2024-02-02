c095faccd03e0845bc3a036cc038093f296510e3
Jacob Kiesel <jake@bitcrafters.co>
2024-02-01 22:13:46 -0700

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 #![windows_subsystem = "windows"]
 
 use std::{
-    env, ffi::OsStr, fs::{self, DirEntry}, io, mem, os::windows::ffi::OsStrExt, path::{Path, PathBuf}, ptr
+    env,
+    ffi::OsStr,
+    fs::{self, DirEntry},
+    io, mem,
+    os::windows::ffi::OsStrExt,
+    path::{Path, PathBuf},
+    process, ptr,
 };
 
 use dirs::data_local_dir;
 use iced::{
+    alignment::{Horizontal, Vertical},
     theme::Theme,
-    widget::{
-        image,
-        pane_grid::{self, Axis},
-        Button, Image, PaneGrid, Text,
-    },
+    widget::{image, Button, Container, Image, Space, Text},
     window::{self},
-    Application, Command, Element, Settings,
+    Application, Command, Element, Length, Settings,
 };
-use windows::{core::PCWSTR, Win32::{self, Foundation::{HANDLE, HINSTANCE, HWND}, System::{Com::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE}, Registry::HKEY}, UI::Shell::SHELLEXECUTEINFOW}};
+use windows::{
+    core::PCWSTR,
+    Win32::{
+        self,
+        Foundation::{HANDLE, HINSTANCE, HWND},
+        Storage::FileSystem::FILE_ATTRIBUTE_NORMAL,
+        System::{
+            Com::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE},
+            Registry::HKEY,
+        },
+        UI::{
+            Controls::{IImageList, ILD_TRANSPARENT},
+            Shell::{
+                SHGetFileInfoW, SHGetImageList, SHELLEXECUTEINFOW, SHFILEINFOW, SHGFI_SYSICONINDEX,
+                SHIL_EXTRALARGE,
+            },
+            WindowsAndMessaging::HICON,
+        },
+    },
+};
+
+const GRID_WIDTH: usize = 6;
 
 pub fn main() -> iced::Result {
     let mut args = env::args().skip(1);
@@ -48,7 +72,7 @@ pub fn main() -> iced::Result {
     } else {
         Launcher::run(Settings {
             window: window::Settings {
-                decorations: false,
+                decorations: true,
                 ..Default::default()
             },
             flags: LauncherFlags {
@@ -61,7 +85,7 @@ pub fn main() -> iced::Result {
 }
 
 struct Launcher {
-    pane_grid_state: pane_grid::State<Option<io::Result<DirEntry>>>,
+    folder_state: Vec<io::Result<DirEntry>>,
     flags: LauncherFlags,
 }
 
@@ -74,7 +98,7 @@ struct LauncherFlags {
 #[derive(Debug, Clone)]
 enum Message {
     Open(PathBuf),
-    Close,
+    OpenFolder,
 }
 
 impl Application for Launcher {
@@ -90,7 +114,7 @@ impl Application for Launcher {
         let state = init_state(&flags);
         (
             Launcher {
-                pane_grid_state: state,
+                folder_state: state,
                 flags,
             },
             Command::none(),
@@ -98,7 +122,16 @@ impl Application for Launcher {
     }
 
     fn title(&self) -> String {
-        String::from("kslauncher")
+        String::from(format!(
+            "kslauncher - {}",
+            self.flags
+                .folder
+                .clone()
+                .unwrap_or_default()
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+        ))
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
@@ -110,7 +143,11 @@ impl Application for Launcher {
                     .chain(Some(0))
                     .collect::<Vec<u16>>();
                 unsafe {
-                    Win32::System::Com::CoInitializeEx(None, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE).unwrap();
+                    Win32::System::Com::CoInitializeEx(
+                        None,
+                        COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE,
+                    )
+                    .unwrap();
                     let mut shell_info = SHELLEXECUTEINFOW {
                         cbSize: mem::size_of::<SHELLEXECUTEINFOW>() as u32,
                         fMask: 0,
@@ -128,13 +165,16 @@ impl Application for Launcher {
                         Anonymous: mem::zeroed(),
                         hProcess: HANDLE::default(),
                     };
-                    Win32::UI::Shell::ShellExecuteExW(
-                        &mut shell_info
-                    ).unwrap();
+                    Win32::UI::Shell::ShellExecuteExW(&mut shell_info).unwrap();
                 };
             }
-            Message::Close => {
-                // Do nothing
+            Message::OpenFolder => {
+                if let Some(folder) = &self.flags.folder {
+                    process::Command::new("explorer.exe")
+                        .arg(folder.display().to_string())
+                        .spawn()
+                        .unwrap();
+                }
             }
         }
         Command::single(iced_runtime::command::Action::Window(
@@ -145,31 +185,73 @@ impl Application for Launcher {
     fn view(&self) -> Element<Message> {
         let content: Element<Message> = match &self.flags.file_move_error {
             Some(e) => Text::new(format!("Failed to add file to launcher folder: {e}")).into(),
-            None => PaneGrid::new(
-                &self.pane_grid_state,
-                |_pane, entry, _maximized| match entry {
-                    Some(Ok(entry)) => {
-                        let file_path = entry.path();
-                        let file_name = file_path
-                            .file_stem()
-                            .unwrap_or_default()
-                            .to_string_lossy()
-                            .to_string();
-                        Button::new(iced::widget::column!(
-                            Image::<image::Handle>::new("bleh"),
-                            Text::new(file_name.clone())
-                        ))
-                        .on_press(Message::Open(file_path))
+            None => iced::widget::Column::with_children(
+                self.folder_state
+                    .chunks(GRID_WIDTH)
+                    .map(|row| {
+                        let empty = (0..(GRID_WIDTH - row.len()))
+                            .map(|_| Space::new(Length::FillPortion(1), Length::Shrink).into());
+                        iced::widget::Row::with_children(
+                            row.iter()
+                                .map(|entry| match entry {
+                                    Ok(entry) => {
+                                        let file_path = entry.path();
+                                        let image_file_path = get_icon(&file_path);
+                                        let file_name = file_path
+                                            .file_stem()
+                                            .unwrap_or_default()
+                                            .to_string_lossy()
+                                            .to_string();
+                                        Container::new(
+                                            Button::new(
+                                                iced::widget::column!(
+                                                    Image::<image::Handle>::new(image_file_path)
+                                                        .content_fit(iced::ContentFit::Contain)
+                                                        .height(Length::Fixed(48.0))
+                                                        .width(Length::Fill),
+                                                    Text::new(file_name.clone())
+                                                        .vertical_alignment(
+                                                            iced::alignment::Vertical::Center
+                                                        )
+                                                        .horizontal_alignment(
+                                                            iced::alignment::Horizontal::Center
+                                                        )
+                                                        .height(Length::FillPortion(1))
+                                                        .width(Length::Fill)
+                                                )
+                                                .align_items(iced::Alignment::Center),
+                                            )
+                                            .on_press(Message::Open(file_path))
+                                            .width(Length::Fill)
+                                            .height(Length::Fill),
+                                        )
+                                        .width(Length::FillPortion(1))
+                                        .height(Length::Fill)
+                                        .align_x(Horizontal::Center)
+                                        .align_y(Vertical::Center)
+                                        .padding(2.0)
+                                        .into()
+                                    }
+                                    Err(e) => Text::new(format!("Failed to read file: {e}")).into(),
+                                })
+                                .chain(empty)
+                                .collect::<Vec<_>>(),
+                        )
+                        .height(Length::FillPortion(1))
                         .into()
-                    }
-                    Some(Err(e)) => Text::new(format!("Failed to read file: {e}")).into(),
-                    None => Text::new("Folder is empty").into(),
-                },
+                    })
+                    .collect::<Vec<_>>(),
             )
             .into(),
         };
-        let close_button = Button::new(Text::new("Close")).on_press(Message::Close);
-        iced::widget::column!(close_button, content).into()
+        let open_folder = Button::new(
+            Text::new("Open Folder in Explorer")
+                .horizontal_alignment(iced::alignment::Horizontal::Center)
+                .width(Length::Fill),
+        )
+        .on_press(Message::OpenFolder)
+        .width(Length::Fill);
+        iced::widget::column!(open_folder, content).into()
     }
 
     fn theme(&self) -> Theme {
@@ -177,31 +259,120 @@ impl Application for Launcher {
     }
 }
 
-fn init_state(flags: &LauncherFlags) -> pane_grid::State<Option<Result<DirEntry, io::Error>>> {
+fn get_icon(file_path: &Path) -> image::Handle {
+    unsafe {
+        let mut psfi = SHFILEINFOW::default();
+        let file_path_wide = file_path
+            .as_os_str()
+            .encode_wide()
+            .chain(Some(0))
+            .collect::<Vec<u16>>();
+        let system_image_list = SHGetFileInfoW(
+            PCWSTR(file_path_wide.as_ptr()),
+            FILE_ATTRIBUTE_NORMAL,
+            Some(&mut psfi),
+            mem::size_of::<SHFILEINFOW>() as u32,
+            SHGFI_SYSICONINDEX,
+        );
+        let extra_large_image_list: IImageList = SHGetImageList(SHIL_EXTRALARGE as i32).unwrap();
+        if system_image_list != 0 {
+            let icon = extra_large_image_list
+                .GetIcon(psfi.iIcon, ILD_TRANSPARENT.0)
+                .unwrap();
+            return icon_to_rgba_image(icon);
+        } else {
+            unimplemented!()
+        }
+    }
+}
+
+unsafe fn icon_to_rgba_image(icon: HICON) -> image::Handle {
+    use std::{mem::MaybeUninit, ptr::addr_of_mut};
+    use windows::Win32::{
+        Graphics::Gdi::{
+            DeleteObject, GetDC, GetDIBits, GetObjectW, ReleaseDC, BITMAP, BITMAPINFOHEADER,
+            BI_RGB, DIB_RGB_COLORS, HDC,
+        },
+        UI::WindowsAndMessaging::GetIconInfo,
+    };
+
+    let bitmap_size = i32::try_from(mem::size_of::<BITMAP>()).unwrap();
+    let biheader_size = u32::try_from(mem::size_of::<BITMAPINFOHEADER>()).unwrap();
+
+    let mut info = MaybeUninit::uninit();
+    GetIconInfo(icon, info.as_mut_ptr()).unwrap();
+    let info = info.assume_init_ref();
+    DeleteObject(info.hbmMask).unwrap();
+
+    let mut bitmap: MaybeUninit<BITMAP> = MaybeUninit::uninit();
+    let result = GetObjectW(info.hbmColor, bitmap_size, Some(bitmap.as_mut_ptr().cast()));
+    assert!(result == bitmap_size);
+    let bitmap = bitmap.assume_init_ref();
+
+    let width = u32::try_from(bitmap.bmWidth).unwrap();
+    let height = u32::try_from(bitmap.bmHeight).unwrap();
+    let w = usize::try_from(bitmap.bmWidth).unwrap();
+    let h = usize::try_from(bitmap.bmHeight).unwrap();
+
+    let buf_size = w
+        .checked_mul(h)
+        .and_then(|size| size.checked_mul(4))
+        .unwrap();
+    let mut buf: Vec<u8> = Vec::with_capacity(buf_size);
+
+    let dc = GetDC(HWND(0));
+    assert!(dc != HDC(0));
+
+    let mut bitmap_info = BITMAPINFOHEADER {
+        biSize: biheader_size,
+        biWidth: bitmap.bmWidth,
+        biHeight: -bitmap.bmHeight,
+        biPlanes: 1,
+        biBitCount: 32,
+        biCompression: BI_RGB.0,
+        biSizeImage: 0,
+        biXPelsPerMeter: 0,
+        biYPelsPerMeter: 0,
+        biClrUsed: 0,
+        biClrImportant: 0,
+    };
+    let result = GetDIBits(
+        dc,
+        info.hbmColor,
+        0,
+        height,
+        Some(buf.as_mut_ptr().cast()),
+        addr_of_mut!(bitmap_info).cast(),
+        DIB_RGB_COLORS,
+    );
+    assert_ne!(result, 0);
+    buf.set_len(buf.capacity());
+
+    let result = ReleaseDC(HWND(0), dc);
+    assert!(result == 1);
+    DeleteObject(info.hbmColor).unwrap();
+
+    for chunk in buf.chunks_exact_mut(4) {
+        let [b, _, r, _] = chunk else { unreachable!() };
+        mem::swap(b, r);
+    }
+
+    image::Handle::from_pixels(width, height, buf)
+}
+
+fn init_state(flags: &LauncherFlags) -> Vec<Result<DirEntry, io::Error>> {
     match &flags.folder {
         Some(folder) => {
             let _ = fs::create_dir_all(folder);
             match fs::read_dir(folder) {
-                Ok(mut read_dir) => {
-                    let (mut state, mut last_pane) = pane_grid::State::new(read_dir.next());
-                    for entry in read_dir {
-                        if let Some((pane, _split)) =
-                            state.split(Axis::Horizontal, &last_pane, Some(entry))
-                        {
-                            last_pane = pane;
-                        }
-                    }
-                    state
-                }
+                Ok(read_dir) => read_dir.collect::<Vec<_>>(),
                 Err(e) => {
-                    let (state, _) = pane_grid::State::new(Some(Err(e)));
-                    state
+                    vec![Err(e)]
                 }
             }
         }
         None => {
-            let (state, _) = pane_grid::State::new(None);
-            state
+            vec![]
         }
     }
 }
